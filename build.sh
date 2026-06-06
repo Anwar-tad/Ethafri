e017a826c5e9943ba9598db6e11815607ae0a566
Anwar-tad <ilvuma11@gmail.com>
2026-06-06 04:35:46 +0300

    Add migration files for market

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# ስህተት ሲያጋጥም እንዲቆም
 set -o errexit
 
-# ፓኬጆችን መጫን
 pip install -r requirements.txt
-
-# ስታቲክ ፋይሎችን መሰብሰብ
 python manage.py collectstatic --no-input
-
-# ዳታቤዙን ማዘጋጀት (ይህ ነው ስህተቱን የሚፈታው!)
 python manage.py migrate
