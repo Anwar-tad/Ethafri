75b8aa0735e5005a26d79ed298eeded9a860317c
Anwar-tad <ilvuma11@gmail.com>
2026-06-16 02:03:44 +0300

    Fix: Add missing columns to Category model

--- marketplace/migrations/0001_initial.py
+++ marketplace/migrations/0001_initial.py
@@ -14,6 +14,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
                 ('slug', models.SlugField(blank=True, unique=True)),
+                ('description', models.TextField(blank=True)), # ይህ ተጨምሯል
+                ('icon', models.CharField(blank=True, max_length=50)), # ይህ ተጨምሯል
             ],
         ),
         migrations.CreateModel(
