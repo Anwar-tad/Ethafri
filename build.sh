1efa689734eca7386bf8a558b67e79a7ae6712b7
Anwar-tad <ilvuma11@gmail.com>
2026-06-16 02:30:48 +0300

    Force fake migration

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-# ከዚህ በፊት የነበረውን python manage.py migrate አጥፋና ይህንን ተካው
-python manage.py migrate --fake marketplace 0002_auto_fix
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# አድሚን እንዲፈጥር ትዕዛዝ መስጠት
+python create_admin.py
