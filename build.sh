e03732666615d45f305cd948bdaa78b30a0a839c
Anwar-tad <ilvuma11@gmail.com>
2026-06-16 17:14:47 +0300

    Standardize build script after manual migration sync

--- build.sh
+++ build.sh
@@ -4,9 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# አድሚን እንዲፈጥር ትዕዛዝ መስጠት
 python create_admin.py
-
-# ከዚህ በፊት የነበረውን python manage.py migrate አጥፋና ይህንን ተካው
-python manage.py migrate --fake marketplace 0002_auto_fix
